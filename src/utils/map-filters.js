2cc9afa3818254f5b8d5b4726447cfd05668e250
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-01-30 22:07:42 +0500

    fix: filter fix and improvements

--- src/utils/map-filters.js
+++ src/utils/map-filters.js
@@ -1,6 +1,6 @@
 const mapFilters =
   (activePrefix, inactivePrefix) =>
   ({ domain, active }) =>
-    active ? `${activePrefix}${domain}^|` : `${inactivePrefix}${domain}^|`;
+    active ? `${activePrefix}${domain}^` : `${inactivePrefix}${domain}^`;
 
 module.exports = mapFilters;
