e1d94c0437a26ac05ac8d7ac94aad15cc99d4e00
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-06-30 02:04:40 +0500

    fix: fix build.sh file
    Former-commit-id: 94284142d94694ee0c6c65cc6d3789209363e620

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-rm -rf converted originals lists.json domains.txt hosts.txt dalisoft.txt
+rm -rf converted originals lists.json dalisoft.txt
 node src/generate.js
 python3 src/convertlists.py lists.json converted/
+rm -rf domains.txt hosts.txt rules.txt
 cat converted/lists/* | sort | uniq >>domains.txt
 node src/compile.js
