7561f6acf2abbc7b069d745921985a5bb11c4681
Davlatjon Shavkatov <dalisoft.dev@gmail.com>
2022-07-09 00:34:55 +0500

    fix: dedupe allowlist
    Former-commit-id: b4e49c4f3716d7b3cfc0228885439e8152d88875

--- src/compile.js
+++ src/compile.js
@@ -1,12 +1,19 @@
 import fs from 'fs/promises';
 import path from 'path';
+import nextdnsConfig from '../nextdns-config.json' assert { type: 'json' };
 import { HOSTS_COMMENT, RULES_COMMENT } from './comments.js';
 
 const domains = (
   await fs.readFile(path.resolve('domains.txt'), { encoding: 'utf-8' })
 )
   .split('\n')
-  .filter((domain) => domain);
+  .filter((domain) => domain)
+  .filter(
+    (domain) =>
+      !nextdnsConfig.allowlist.find(
+        ({ domain: allowDomain, active }) => allowDomain === domain && active
+      )
+  );
 
 const hosts = domains.map((domain) => `0.0.0.0 ${domain}`);
 const rules = domains.map((domain) => `||${domain}^`);
